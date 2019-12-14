d240efa6b89c39287e0855498a5f3216aac370ec
oyasumi <k.anuchit@kkumail.com>
2019-12-15 00:46:15 +0700

    aaa

--- client/src/components/Article.js
+++ client/src/components/Article.js
@@ -16,16 +16,29 @@ export default class Article extends Component {
   }
   componentWillUnmount() {}
   getData = () => {
-    Axios.get("/").then(res => {
+    Axios.get("/article").then(res => {
       this.setState({ data: res.data });
     });
   };
-  
+  renderData = ({ artID, artName }) => (
+    <Row>
+      <Col xs={12}>
+        <Card key={artID} className="MarginTop">
+          <Card.Body>
+            <Link to={`/article/${artID}`}>
+              <A href="#">{artName}</A>
+            </Link>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  ); 
+    
   render() {
     const { data } = this.state;
     return (
       <div>
-        <Container>{data}</Container>
+        <Container>{data.map(this.renderData)}</Container>
       </div>
     );
   }
