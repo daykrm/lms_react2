2fac500820c0ddb2b868a02d2a33ac86f046ecda
oyasumi <k.anuchit@kkumail.com>
2019-12-15 12:32:21 +0700

    '/'

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
-require("dotenv").config();
+//require("dotenv").config();
 const bodyParser = require("body-parser");
 //const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
